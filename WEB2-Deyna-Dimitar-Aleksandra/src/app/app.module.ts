ed8132f2be5205378df80d64816c6abf589bd2aa
Petrov,Dimitar D.D <dimitar.petrov@student.fontys.nl>
2020-03-09 14:33:01 +0100

    added routing.

--- WEB2-Deyna-Dimitar-Aleksandra/src/app/app.module.ts
+++ WEB2-Deyna-Dimitar-Aleksandra/src/app/app.module.ts
@@ -5,7 +5,12 @@ import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +20,13 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
+    , MatSliderModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
