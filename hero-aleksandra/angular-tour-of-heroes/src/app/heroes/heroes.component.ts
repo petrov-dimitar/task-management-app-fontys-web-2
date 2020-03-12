a844a8ba83758ac8120f18186be62719d138b230
Belcheva,Aleks A.E <a.belcheva@student.fontys.nl>
2020-03-12 18:31:28 +0100

    Step 3/4 heroes tutorial

--- hero-aleksandra/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ hero-aleksandra/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 //Improt the hero as an object
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -15,16 +17,22 @@ export class HeroesComponent implements OnInit {
   //hero: Hero = {
     //id: 1,
     //name: 'Windstorm'
-    heroes = HEROES;
+    heroes: Hero[];
 
-   constructor() { }
+   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
    ngOnInit() {
+     this.getHeroes(); 
    }
    selectedHero: Hero;
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
 
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+  this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
 
