37aaa919579cb0d0391db72261b818cd33c32b2c
Petrov,Dimitar D.D <dimitar.petrov@student.fontys.nl>
2020-03-17 10:12:57 +0200

    Implemented one search which works for tasks/departments/employees.

--- WEB2-Deyna-Dimitar-Aleksandra/src/app/app.component.ts
+++ WEB2-Deyna-Dimitar-Aleksandra/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { DepartmentsService } from './services/departments.service';
 import { Task } from './interfaces/task';
 import { Employee } from './interfaces/employee';
 import { Department } from './interfaces/department';
+import { ManagementEntity } from './interfaces/managementEntity';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -21,19 +22,29 @@ export class AppComponent implements OnInit {
   constructor(public taskService: TaskService, private employeeService: EmployeeService, private departmentService: DepartmentsService) { }
 
   myControl = new FormControl();
-  options: Task[] = [];
-  filteredOptions: Observable<Task[]>;
+  options: ManagementEntity[] = [];
+  filteredOptions: Observable<ManagementEntity[]>;
 
   tasks: Task[];
   employees: Employee[];
   departments: Department[];
+
+  stringTypeTask: string = 'task';
+  stringTypeEmployee: string = 'employee';
+  stringTypeDepartment: string = 'department';
 
   ngOnInit() {
     this.getTasks();
     this.getEmployees();
     this.getDepartments();
     this.tasks.filter(task => {
-      this.options.push(task);
+      this.options.push(new ManagementEntity(task.id, task.name, this.stringTypeTask));
+    })
+    this.employees.filter(employee => {
+      this.options.push(new ManagementEntity(employee.id, employee.name, this.stringTypeEmployee));
+    })
+    this.departments.filter(department => {
+      this.options.push(new ManagementEntity(department.id, department.name, this.stringTypeDepartment));
     })
 
     this.filteredOptions = this.myControl.valueChanges
@@ -54,7 +65,7 @@ export class AppComponent implements OnInit {
   getDepartments(): void {
     this.departmentService.getDepartments().subscribe(dep => this.departments = dep);
   }
-  private _filter(value: string): Task[] {
+  private _filter(value: string): ManagementEntity[] {
     const filterValue = value.toLowerCase();
 
     return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
