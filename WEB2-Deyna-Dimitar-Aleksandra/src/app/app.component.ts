ffea550ecfe45d49ffb92f87a9235b76dae67594
Petrov,Dimitar D.D <dimitar.petrov@student.fontys.nl>
2020-03-28 11:45:51 +0200

    added hamburger menu button and sidenav functionality, which closes the nav menu on mobile.

--- WEB2-Deyna-Dimitar-Aleksandra/src/app/app.component.ts
+++ WEB2-Deyna-Dimitar-Aleksandra/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 
@@ -11,6 +11,7 @@ import { Task } from './interfaces/task';
 import { Employee } from './interfaces/employee';
 import { Department } from './interfaces/department';
 import { ManagementEntity } from './interfaces/managementEntity';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -18,8 +19,8 @@ import { ManagementEntity } from './interfaces/managementEntity';
 })
 export class AppComponent implements OnInit {
   title = 'WEB2-Deyna-Dimitar-Aleksandra';
-
-  constructor(public taskService: TaskService, private employeeService: EmployeeService, private departmentService: DepartmentsService) { }
+  @ViewChild('drawer') drawer: any;
+  constructor(public taskService: TaskService, private employeeService: EmployeeService, private departmentService: DepartmentsService, private breakpointObserver: BreakpointObserver) { }
 
   myControl = new FormControl();
   options: ManagementEntity[] = [];
@@ -70,4 +71,12 @@ export class AppComponent implements OnInit {
 
     return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
   }
+  closeSideNav() {
+    if (this.drawer._mode == 'over') {
+      this.drawer.close();
+    }
+  }
+  public isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map((result: BreakpointState) => result.matches));
 }
